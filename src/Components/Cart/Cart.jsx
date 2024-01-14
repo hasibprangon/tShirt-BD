180b4fc8e198513cf1d12550b247fd08fea97ee6
hasibprangon <hasibulhossainp09@gmail.com>
2024-01-14 23:46:24 +0600

    conditional rendering system 1

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 const Cart = ({cart, handleRemoveFromCart}) => {
+    let message;
+    if(cart.length === 0){
+        message = <p>Please add atleast one product</p>
+    }
+    else{
+        message = <div>
+            <h3>Thanks</h3>
+            <p>For buy something from us</p>
+        </div>
+    }
     return (
         <div>
             <h2>Order Summary: {cart.length}</h2>
+            {message}
             {
                 cart.map(tShirt => <p
                 key={tShirt._id}
@@ -16,3 +27,8 @@ const Cart = ({cart, handleRemoveFromCart}) => {
 };
 
 export default Cart;
+
+/*
+SYSTEM OF CONDITIONAL RENDERIGN
+S1:use if else to set a variable that will contain an element, components or anything
+*/
