bc2d19258acca9d24cd7072c8ea0cef57c5336a7
hasibprangon <hasibulhossainp09@gmail.com>
2024-01-14 23:29:00 +0600

    adding toast

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,19 +3,28 @@ import { useLoaderData } from 'react-router-dom';
 import TShirt from '../TShirt/TShirt';
 import Cart from '../Cart/Cart';
 import './Home.css'
+import toast from 'react-hot-toast';
+
 
 const Home = () => {
     const tShirts = useLoaderData();
-   const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState([]);
 
-    const handleAddToCart = (tShirts) =>{
-       const newCart=  [...cart, tShirts];
-       setCart(newCart);
+    const handleAddToCart = (tShirts) => {
+        const exists = cart.find(ts => ts._id === tShirts._id);
+        if (exists) {
+            toast('This product already added')
+        }
+        else {
+
+            const newCart = [...cart, tShirts];
+            setCart(newCart);
+        }
     }
 
-    const handleRemoveFromCart = id =>{
+    const handleRemoveFromCart = id => {
         const remaining = cart.filter(ts => ts._id !== id);
-        setCart(remaining); 
+        setCart(remaining);
     }
     return (
         <div className='home-container'>
@@ -24,14 +33,14 @@ const Home = () => {
                     tShirts.map(tShirt => <TShirt
                         key={tShirt._id}
                         tShirt={tShirt}
-                        handleAddToCart ={handleAddToCart}
+                        handleAddToCart={handleAddToCart}
                     ></TShirt>)
                 }
             </div>
             <div className='cart-container'>
-                <Cart 
-                cart={cart}
-                handleRemoveFromCart = {handleRemoveFromCart}
+                <Cart
+                    cart={cart}
+                    handleRemoveFromCart={handleRemoveFromCart}
                 ></Cart>
             </div>
         </div>
--- src/Components/LayOut/Default.jsx
+++ src/Components/LayOut/Default.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
 
 const Default = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Toaster />
         </div>
     );
 };
