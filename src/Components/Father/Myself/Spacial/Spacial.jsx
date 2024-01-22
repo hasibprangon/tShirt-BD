1afe735d2a97d9d48762fa512b857bba8edf1d2c
hasibprangon <hasibulhossainp09@gmail.com>
2024-01-22 20:46:07 +0600

    introduction to context API

--- src/Components/Father/Myself/Spacial/Spacial.jsx
+++ src/Components/Father/Myself/Spacial/Spacial.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../../../GrandPa/GrandPa';
 
 const Spacial = ({ring}) => {
+
+    const GldnRing = useContext(RingContext);
     return (
         <div>
             <h3>Spacial</h3>
             <p><small>{ring}</small></p>
+            <p><small>{GldnRing}</small></p>
         </div>
     );
 };
--- src/Components/GrandPa/GrandPa.jsx
+++ src/Components/GrandPa/GrandPa.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import Aunty from '../Aunty/Aunty';
 import './GrandPa.css'
-
+export const RingContext = createContext('gold')
 const GrandPa = () => {
     const ring = 'diamond';
+
     return (
         <div className='grand-pa'>
             <h3>grandpa</h3>
-            <section className='flex'>
-                <Father ring={ring}></Father>
-                <Uncle></Uncle>
-                <Aunty></Aunty>
-            </section>
+            <RingContext.Provider value='golden ring'>
+                <section className='flex'>
+                    <Father ring={ring}></Father>
+                    <Uncle></Uncle>
+                    <Aunty></Aunty>
+                </section>
+            </RingContext.Provider>
         </div>
     );
 };
 
 export default GrandPa;
+
+
+/**
+ * 1: create context
+*/
